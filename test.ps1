a65b102ad4235e0073988f4d88984f36d083075b
MrAndi Scripted <78992535+MrAndiGamesDev@users.noreply.github.com>
2025-10-13 19:07:02 -0400

    Delete test.ps1

--- test.ps1
+++
@@ -1,6 +0,0 @@
-# Create a self-signed certificate valid for 1 year
-$cert = New-SelfSignedCertificate -Type CodeSigningCert -Subject "CN=MrAndi Scripted" -CertStoreLocation Cert:\CurrentUser\My -KeyExportPolicy Exportable -NotAfter (Get-Date).AddYears(1)
-
-# Export to PFX
-$pswd = ConvertTo-SecureString -String "2006" -Force -AsPlainText
-Export-PfxCertificate -Cert $cert -FilePath "certificate.pfx" -Password $pswd
